78412289f97a70ad6f82cbafdeadc59544c9fdcc
Jesus F. Vazquez Biltre <jesusfvb2@gmail.com>
2022-03-26 12:07:06 -0400

    Eliminada carpeta frontend

--- frontend/src/App/hooks/Message.tsx
+++
@@ -1,8 +0,0 @@
-import {useSnackbar} from "notistack";
-
-export const useErrorMessage = (textDefault: string = "Datos no validos") => {
-    const {enqueueSnackbar} = useSnackbar();
-    return (text: string = textDefault): void => {
-        enqueueSnackbar(text, {variant: "error"})
-    }
-}
